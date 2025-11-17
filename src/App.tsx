8b7a2d57ed7f410cd4016f4cdbfd51b885a6dc65
Darwin Tandazo <darwinsttandazo@gmail.com>
2025-11-17 02:38:24 -0500

    fix: eliminar imports de React no usados

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Header } from './components/Header';
 import { VideoDownloader } from './components/VideoDownloader';
 import { Footer } from './components/Footer';
